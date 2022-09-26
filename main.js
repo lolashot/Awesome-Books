16482cc2f6f4ce9e8986da2742ae0f5de39409c3
lolashot <ololadeshot@gmail.com>
2022-09-26 11:55:18 +0100

    linter corrections

--- main.js
+++ main.js
@@ -5,11 +5,32 @@ const msg = document.getElementById('msg');
 const books = document.getElementById('books');
 const add = document.getElementById('add');
 
-let acceptData = () => {
+let data = [{}];
+
+const acceptData = () => {
   data.push({
     text: textInput.value,
     date: dateInput.value,
   });
+
+  const createBooks = () => {
+    books.innerHTML = '';
+    data.forEach((x, y) => {
+      (books.innerHTML += `
+      <div id=${y}>
+            <p>${x.text}</p>
+            <p>${x.date}</p>
+            <div class='options'>
+            <button class='' onClick ='deleteTask(this);createBooks()' class=''>Remove
+            </button>
+            </div>
+            <hr/>
+          </div>
+      `)
+  });
+  
+    resetForm();
+  };
 
   localStorage.setItem('data', JSON.stringify(data));
   createBooks();
@@ -30,30 +51,9 @@ form.addEventListener('submit', (e) => {
   formValidation();
 });
 
-let data = [{}];
-
 const resetForm = () => {
   textInput.value = '';
   dateInput.value = '';
-};
-
-const createBooks = () => {
-  books.innerHTML = '';
-  data.forEach((x, y) => {
-    (books.innerHTML += `
-    <div id=${y}>
-          <p>${x.text}</p>
-          <p>${x.date}</p>
-          <div class='options'>
-          <button class='' onClick ='deleteTask(this);createBooks()' class=''>Remove
-          </button>
-          </div>
-          <hr/>
-        </div>
-    `)
-});
-
-  resetForm();
 };
 
 const deleteTask = (e) => {
