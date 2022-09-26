1fd45fd07b6f75afbbb306c4cafd34d8b7a07004
lolashot <ololadeshot@gmail.com>
2022-09-26 10:44:04 +0100

    linter corrections

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-let form = document.getElementById("form");
-let textInput = document.getElementById("titleInput");
-let dateInput = document.getElementById("authorInput");
-let textarea = document.getElementById("textarea");
-let msg = document.getElementById("msg");
-let books = document.getElementById("books");
-let add = document.getElementById("add");
+const form = document.getElementById('form');
+const textInput = document.getElementById('titleInput');
+const dateInput = document.getElementById('authorInput');
+const msg = document.getElementById('msg');
+const books = document.getElementById('books');
+const add = document.getElementById('add');
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -12,11 +11,10 @@ form.addEventListener("submit", (e) => {
 });
 
 let formValidation = () => {
-  if (textInput.value === "") {
-    console.log("failure");
-    msg.innerHTML = "Title cannot be blank";
+  if (textInput.value === '') {
+    msg.innerHTML = 'Title cannot be blank';
   } else {
-    msg.innerHTML = "";
+    msg.innerHTML = '';
     acceptData();
     add.click();
   }
@@ -30,22 +28,20 @@ let acceptData = () => {
     date: dateInput.value,
   });
 
-  localStorage.setItem("data", JSON.stringify(data));
-
-  console.log(data);
+  localStorage.setItem('data', JSON.stringify(data));
   createBooks();
 };
 
 let createBooks = () => {
-  books.innerHTML = "";
+  books.innerHTML = '';
   data.map((x, y) => {
     return (books.innerHTML += `
     <div id=${y}>
           <p>${x.text}</p>
           <p>${x.date}</p>
 
-          <div class="options">
-          <button class="" onClick ="deleteTask(this);createBooks()" class="">Remove
+          <div class='options'>
+          <button class='' onClick ='deleteTask(this);createBooks()' class=''>Remove
           </button>
           </div>
           <hr/>
@@ -59,18 +55,16 @@ let createBooks = () => {
 let deleteTask = (e) => {
   e.parentElement.parentElement.remove();
   data.splice(e.parentElement.parentElement.id, 1);
-  localStorage.setItem("data", JSON.stringify(data));
-  console.log(data);
-  
+  localStorage.setItem('data', JSON.stringify(data));
 };
 
 let resetForm = () => {
-  textInput.value = "";
-  dateInput.value = "";
+  textInput.value = '';
+  dateInput.value = '';
 };
 
 (() => {
-  data = JSON.parse(localStorage.getItem("data")) || []
+  data = JSON.parse(localStorage.getItem('data')) || []
   console.log(data);
   createBooks();
 })();
