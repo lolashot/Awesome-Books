22e418947fcf12f6ff2bf52220e9cc692cbc2d1d
NurkaAmre <107000157+NurkaAmre@users.noreply.github.com>
2022-09-26 18:37:16 +0600

    Parse data to JSON

--- main.js
+++ main.js
@@ -62,4 +62,9 @@ const deleteTask = (e) => {
   localStorage.setItem('data', JSON.stringify(data));
 };
 
+(() => {
+  data = JSON.parse(localStorage.getItem('data')) || [];
+  createBooks();
+  deleteTask({});
+})();
 
